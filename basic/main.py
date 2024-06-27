25276423887387dc3bbaa990983ca4cf24759b64
AjinkyaSurvase88 <ajinkyasurvase88@gmail.com>
2024-06-27 08:43:17 +0530

    all task functions are added

--- basic/main.py
+++ basic/main.py
@@ -23,3 +23,39 @@ clf= MultinomialNB()
 model=make_pipeline(vectorizer,clf)
 model.fit(tasks['description'],tasks['priority'])
  
+# fuction adding task to list
+def add_task(description,priority):
+    global tasks
+    new_task=pd.DataFrame({'description':[description],'priority':priority})
+    tasks=pd.concat([tasks,new_task],ignore_index=True)
+    save_tasks()
+    
+# function to remove task
+def remove_task(description):
+    tasks=tasks[tasks['description'] !=description]
+    save_tasks()
+
+# list the tasks
+def list_tasks():
+    if tasks.empty:
+        print("no task available")
+    else:
+        print(tasks)
+    
+# Function to recommend task based on ML
+def recommend_task():
+    if not tasks.empty:
+        #get high-priority tasks
+        high_priority_tasks = tasks[tasks['priority']=='High']
+        
+        if not high_priority_tasks.empty:
+            # choose random high priority task
+            random_task = random.choice(high_priority_tasks['description'])
+            print(f"Recommend task: {random_task} - priority :'high")
+            
+        else:
+            print("no high priority task available")
+            
+    else:
+        print("no task available for recommendation")
+
