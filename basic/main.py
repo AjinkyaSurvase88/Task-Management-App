48e454b4b16182a5931adb5c7cd5278d70c16473
AjinkyaSurvase88 <ajinkyasurvase88@gmail.com>
2024-06-27 09:44:31 +0530

    main file created

--- basic/main.py
+++ basic/main.py
@@ -18,10 +18,12 @@ def save_tasks():
     tasks.to_csv('tasks.csv', index=False)
     
 # train the task priority classifier
+
 vectorizer = CountVectorizer()
-clf= MultinomialNB()
-model=make_pipeline(vectorizer,clf)
-model.fit(tasks['description'],tasks['priority'])
+clf = MultinomialNB()
+model = make_pipeline(vectorizer, clf)
+model.fit(tasks['description'], tasks['priority'])
+
  
 # fuction adding task to list
 def add_task(description,priority):
@@ -32,6 +34,7 @@ def add_task(description,priority):
     
 # function to remove task
 def remove_task(description):
+    global tasks
     tasks=tasks[tasks['description'] !=description]
     save_tasks()
 
@@ -50,7 +53,7 @@ def recommend_task():
         
         if not high_priority_tasks.empty:
             # choose random high priority task
-            random_task = random.choice(high_priority_tasks['description'])
+            random_task = random.choice(high_priority_tasks['description'].tolist())
             print(f"Recommend task: {random_task} - priority :'high")
             
         else:
@@ -58,4 +61,38 @@ def recommend_task():
             
     else:
         print("no task available for recommendation")
+        
+# Main menu 
+while True:
+    print("\nTask Management App")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. List Task")
+    print("4. Recommend Task")
+    print("5. Exit")
+    
+    choice=input("select an option: ")
+    
+    if choice=="1":
+        description=input("Enter a task description: ")
+        priority = input("Enter task priority (Low/Medium/High): ").capitalize()
+        add_task(description,priority)
+        print("Task added succesfully")
+        
+    elif choice =="2":
+        description=input("Enter a task to remove: ")
+        remove_task(description)
+        print("Task removed succesfully")
+        
+    elif choice =="3":
+        list_tasks()
+    
+    elif choice =="4":
+        recommend_task()
+        
+    elif choice =="5":
+        print("Good bye!")
+        break
+    else:
+        print("Invalid Option. Please select valid option. ")
 
